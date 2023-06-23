1b4edf2d609998f92a95eee1fda6066722c9a7a6
NrTregges <136847458+NrTregges@users.noreply.github.com>
2023-06-23 14:53:08 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,36 @@
+"""
+Programa Dicionário
+Descrição: Programa que apresenta a estrutura de dados "Dicionário" em Python
+para fins de aprendizado.
+Autor: Nicholas R. Tregges.
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+
+
+# Atribuição de Variáveis
+tabela = {"Arroz": 0.5, "Batata" : 0.2, "Carne" : 0.1}
+
+
+
+
+# Entrada de dados
+
+
+
+
+
+# Processamento de dados
+
+
+
+
+
+# Saída de dados
+print(tabela["Arroz"])
+print(tabela["Batata"])
+
+# print(tabela{"Alface"})
+# print(tabela["Alface"])
+
